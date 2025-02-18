6f8626669d64ed764ec42872d151aea99659641e
Quentin Vansteene <vansteene.quentin@gmail.com>
2025-02-18 16:32:58 +0100

    add coverflow slider styles and scripts

--- altradbenelux2.js
+++ altradbenelux2.js
@@ -1,4 +1,22 @@
 $(function(){
+	// slider/carousel management
+	$('.sliderFW.coverflow').each(function(index,item){
+		setTimeout(function(){
+			var slider = $(item).sliderFW('get');
+			slider.$el.find('.sliderFW__arrow').appendTo(slider.$el);
+
+			slider.content.items.on('click',function(e){
+				if(!$(this).hasClass('active')){
+					var index = $(this).index();
+					if($(this).index() < slider.content.items.filter('.active').index())
+						slider.goToPrev();
+					else
+						slider.goToNext();
+				}
+			});
+			slider.onResize();
+		},1);
+	});
 
 });
 
